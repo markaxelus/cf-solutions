645b807c197c7ef00c218bde475b070ca1b3a9b3
markaxelus <mrkaxelus@gmail.com>
2026-05-16 13:27:31 -0700

    281A sol

---
+++ 281A.cpp
@@ -0,0 +1,17 @@
+// Word Capitalization
+
+#include <iostream>
+using namespace std;
+
+int main () {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  string s;
+  cin >> s;
+
+  s[0] = toupper(s[0]);
+  cout << s;
+
+  return 0;
+}
