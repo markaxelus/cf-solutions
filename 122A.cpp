9f97966769042d9c5c1cb01347d92281d49d5943
markaxelus <mrkaxelus@gmail.com>
2026-05-16 17:38:10 -0700

    122A sol

--- 122A.cpp
+++ 122A.cpp
@@ -1,13 +1,30 @@
-// 
+// Lucky Division
 
 #include <iostream>
 using namespace std;
+
+bool isLucky(int n) {
+  while (n) {
+    if (!(n % 10 == 4 || n % 10 == 7)) {
+      return false;
+    }
+    n /= 10;
+  }
+  return true;
+}
 
 int main () {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  
+  int n;
+  cin >> n;
+
+  if (isLucky(n) || n % 4 == 0 || n % 7 == 0 || n % 47 == 0 ) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
   
   return 0;
 }
