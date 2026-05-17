3351ae843d9bad9108078df9119791c56a5b281f
markaxelus <mrkaxelus@gmail.com>
2026-05-17 01:35:28 -0700

    237A sol

--- 237A.cpp
+++ 237A.cpp
@@ -1,4 +1,4 @@
-// 
+// Free Cash
 
 #include <iostream>
 using namespace std;
@@ -7,6 +7,29 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
+  int n;
+  cin >> n;
+
+  int max_cash = 1;
+  int cur_cash = 1;
+
+  int prev_h, prev_m;
+  cin >> prev_h >> prev_m;
+  int h,m;
+
+  while(--n) {
+    cin >> h >> m;
+    if (h == prev_h && m == prev_m) {
+      cur_cash++;
+    } else {
+      cur_cash = 1;
+    }
+    max_cash = max(max_cash, cur_cash);
+    prev_h = h;
+    prev_m = m;
+  }
+
+  cout << max_cash << endl;
 
   return 0;
 }
