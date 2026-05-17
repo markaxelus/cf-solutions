5a29d81e5546b8829511cc2b3974820d343c457a
markaxelus <mrkaxelus@gmail.com>
2026-05-16 17:14:18 -0700

    1A sol

--- 1A.cpp
+++ 1A.cpp
@@ -1,13 +1,17 @@
-// 
+// Theatre Square
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main () {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  
-  
+  long long n,m,a;
+  cin >> n >> m >> a;
+
+  cout << (long long)(ceil((double) n / a) * ceil((double) m/a)) << "\n";
+
   return 0;
 }
