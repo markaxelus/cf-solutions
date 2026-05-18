8f24b43984c0d1783b2704e38e223abead25653e
markaxelus <mrkaxelus@gmail.com>
2026-05-18 05:26:52 -0700

    467A sol

--- 467A.cpp
+++ 467A.cpp
@@ -7,7 +7,22 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  
+  int n;
+  cin >> n;
+
+  int p, q;
+  int count = 0;
+
+  while(n--) {
+    cin >> p >> q;
+    if ((q-p) < 2) {
+      continue;
+    } else {
+      count++;
+    }
+  }
+
+  cout << count << endl;
 
   return 0;
 }
